bab07bd55c3f39d2c5effbc7464c8f6f3a81f50d
Meghna Shanker <mshanker823@gmail.com>
2019-02-28 12:57:16 -0500

    Created Shield and Hero Script
    Created shield and added code to hero script. Created 3 enemies.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour {
+    static public Hero S;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+    private void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
 }
