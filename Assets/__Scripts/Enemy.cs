b566d0c663ecc339a31eef26a19b32ebb3650007
Meghna Shanker <mshanker823@gmail.com>
2019-02-28 15:27:08 -0500

    Coded Projectile
    Made projectiles spawn using space bar and allowed projectiles to delete enemies.

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -46,4 +46,18 @@ public class Enemy : MonoBehaviour
         pos = tempPos;
     }
 
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.tag == "ProjectileHero")
+        {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
+
 }
