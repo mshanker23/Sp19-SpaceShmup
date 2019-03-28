fc30f2362993424ee19f3e028cfb7c6dd95c74b3
Meghna Shanker <mshanker823@gmail.com>
2019-03-28 19:15:43 -0400

    Enemied drop powerUps
    Added code to make enemies drop power ups. need to fix projectile issue.

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -11,6 +11,7 @@ public class Enemy : MonoBehaviour
     public float health = 10;
     public int score = 100;
     public float showDamageDuration = 0.1f;
+    public float powerUpDropChance = 1f;
 
     [Header("Set Dynamically: Enemy")]
     public Color[] originalColors;
@@ -72,6 +73,7 @@ public class Enemy : MonoBehaviour
         GameObject otherGO = coll.gameObject;
         switch (otherGO.tag)
         {
+
             case "ProjectileHero":
                 Projectile p = otherGO.GetComponent<Projectile>();
                 if (!bndCheck.isOnScreen)
@@ -85,6 +87,11 @@ public class Enemy : MonoBehaviour
                 health -= Main.GetWeaponDefinition(p.type).damageOnHit;
                 if (health <= 0)
                 {
+                    if (!notifiedOfDestruction)
+                    {
+                        Main.S.ShipDestroyed(this);
+                    }
+                    notifiedOfDestruction = true;
                     Destroy(this.gameObject);
                 }
                 Destroy(otherGO);
--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -28,6 +28,7 @@ public class Hero : MonoBehaviour {
         if (S == null)
         {
             S = this;
+        
         }
         else
         {
